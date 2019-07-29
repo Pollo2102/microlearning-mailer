2e894af7ded79fc500694b92d2ad196a27618a57
Diego Valenzuela <valenv.dg@gmail.com>
2019-07-29 00:08:46 -0600

    Added listener

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
 const { getUsers, getLinks, sendMail } = require('./mailer/requests');
+const express = require('express');
+const app = express();
 
 
+app.set('port', process.env.PORT || 3120);
+app.get('/', (req, res) => {
+    res.send('Microlearning mailer');
+});
+app.listen(app.get('port'), () => {
+    console.log(`App listening on port ${app.get('port')}`);
+});
 
 setInterval(() => {
     getUsers()
