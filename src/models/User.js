5025fb8fae6de2c376f84757191d8197ba08bc05
Diego Valenzuela <valenv.dg@gmail.com>
2019-07-28 23:50:22 -0600

    Removed unnecesary files, made code cleaner, fixed errors.

--- src/models/User.js
+++
@@ -1,9 +0,0 @@
-class User {
-    constructor(fullname, email, subscriptions) {
-        this.fullname = fullname;
-        this.email = email;
-        this.subscriptions = subscriptions;
-    }
-}
-
-module.exports = User;
